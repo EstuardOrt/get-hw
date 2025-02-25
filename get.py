ed797c0458badc7db73a0c197a4ee5dc3f7360d6
David Mejicano <demoortiz1@gmail.com>
2025-02-25 12:28:59 -0600

    Primer commit

---
+++ get.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/hello', methods=['GET'])
+def get_hello():
+    return "Hello Word"
+
+if __name__ == '__main__':
+    app.run(debug=True)
